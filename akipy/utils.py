945dc093a2329e3c8b80803b8c794abef24aa7bf
TrueSaiyan <ufperth@protonmail.com>
2024-07-28 00:59:57 +0800

    Update utils.py

--- akipy/utils.py
+++ akipy/utils.py
@@ -1,16 +1,17 @@
+from typing import Union
 import httpx
 
 from .dicts import ANSWERS, HEADERS
 
 
-def request_handler(url: str, method: str, data: dict | None = None) -> httpx.Response:
+def request_handler(url: str, method: str, data: Union[dict, None] = None) -> httpx.Response:
     if method == 'GET':
         return httpx.get(url, headers=HEADERS, timeout=30.0)
     elif method == 'POST':
         return httpx.post(url, headers=HEADERS, data=data, timeout=30.0)
 
 
-async def async_request_handler(url: str, method: str, data: dict | None = None) -> httpx.Response:
+async def async_request_handler(url: str, method: str, data: Union[dict, None] = None) -> httpx.Response:
     if method == 'GET':
         async with httpx.AsyncClient() as client:
             return await client.get(url, headers=HEADERS, timeout=30.0)
@@ -19,7 +20,7 @@ async def async_request_handler(url: str, method: str, data: dict | None = None)
             return await client.post(url, headers=HEADERS, data=data, timeout=30.0)
 
 
-def get_answer_id(ans: str | int):
+def get_answer_id(ans: Union[str, int]):
     for key, values in ANSWERS.items():
         if ans in values:
             return key
